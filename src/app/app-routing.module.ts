dc591cf5cab9537c824c4f81288543dfd762cfd9
Mamadou-cherif <barrymamadoucherif289@gmail.com>
2021-05-05 18:01:31 +0000

    trial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes: Routes = [
 
-{path:"about", component: AboutComponent},
-{path:"contact", component:ContactComponent},
-{path:"", redirectTo: 'HeaderComponent', pathMatch: 'full'},
-{path:"portfolio", component:PortfolioComponent},
 
 
 ];
